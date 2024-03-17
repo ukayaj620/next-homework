35638de209b9bb6fb16fedae8bd5d0762d4958de
Jayaku Briliantio <jayakubriliantio@gmail.com>
2024-03-17 22:41:36 +0700

    feat: Initial commit (next homework)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
+import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${inter.variable} font-sans w-screen`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
